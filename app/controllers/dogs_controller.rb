5d1b8a7c5d9b3ed64bdf962f150286d6c82b837e
Karl Keller <kgk86@hotmail.com>
2021-01-29 17:53:46 +0100

    add actions for show and new + create

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,4 +2,35 @@ class DogsController < ApplicationController
   def index
     @dogs = Dog.all
   end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    #TODO
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = dog.new(dog_params)
+    @dog.user = current_user
+    if @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  #TODO
+  def set_dog
+  end
+
+  # strong params - only allow the white list through.
+  def dog_params
+    params.require(:dog)
+      .permit(:name, :breed, :size, :categroy, :description, :hypoallergenic, :city)
+  end
 end
