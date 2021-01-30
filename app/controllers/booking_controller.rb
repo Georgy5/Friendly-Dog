854900fc796b0ee8a107affec91483f5171b6e5f
Karl Keller <kgk86@hotmail.com>
2021-01-30 14:23:25 +0100

    create create action

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,9 +1,23 @@
 class BookingController < ApplicationController
   def new
-    @dog = Dog.find(params[:cocktail_id])
+    @dog = Dog.find(params[:dog_id])
     @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @dog = dog.find(params[:dog_id])
+    @booking.dog = @dog
+    if @booking.save
+      redirect_to booking_new_path(@dog)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :status, :user_id, :dog_id)
   end
 end
