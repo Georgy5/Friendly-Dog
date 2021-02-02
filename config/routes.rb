44444c9928b16171653c15d3bc50fcdb0afc9cfc
Karl Keller <kgk86@hotmail.com>
2021-02-02 20:22:36 +0100

    Corrected the Booking routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'booking/new'
-  get 'booking/create'
+  get 'bookings/new'
+  get 'bookings/create'
+  #get 'booking/new'
+  #get 'booking/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
 
   devise_for :users
 
-  resources :dogs, only: [:index, :show, :new, :create, :destroy]
+  resources :dogs, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookings, only: [:new, :create]
+  end
 end
