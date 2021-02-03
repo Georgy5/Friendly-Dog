e1ece66bf96ac89b4c998eff93203f4a18e628d5
peter huyser <huyserp@gmail.com>
2021-02-03 19:38:04 +0100

    update create dog form to accomodate new seed info, collection: CATEGORIES

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,9 +3,10 @@ class Dog < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
+  CATEGORIES = ['guide dog', 'emotional support', 'kid-friendly', 'search and rescue']
   validates :name, presence: true
   validates :breed, presence: true
   validates :size, presence: true, inclusion: { in: ['small', 'med-small', 'medium', 'med-large', 'large'] }
-  validates :category, presence: true, inclusion: { in: ['guide dog', 'emotional support', 'kid-friendly', 'search and rescue'] }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :city, presence: true
 end
