4a72792cc732f2946830e143b7256cba75bc429a
Mark Mounsey <mark.mounsey0512@gmail.com>
2021-02-04 17:44:30 +0100

    change city in dog model to address

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,6 @@
 class Dog < ApplicationRecord
-  geocoded_by :city
-  after_validation :geocode, if: :will_save_change_to_city?
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
