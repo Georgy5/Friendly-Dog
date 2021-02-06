6576f04da89edc696b99e8450f61b25f70bf789d
Karl Keller <kgk86@hotmail.com>
2021-02-06 14:48:04 +0100

    resolve conflict between mapbox and flatpickr in favor of mapbox

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,6 +3,3 @@ import flatpickr from "flatpickr";
 flatpickr(".datepicker", {
   altInput: true
 });
-
-// Initialize Flatpickr with Rails Turbolinks
-document.addEventListener('turbolinks:load', start)
