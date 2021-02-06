50e1001439d5e6bccd626885538a116afdd4fa5c
peter huyser <huyserp@gmail.com>
2021-02-06 08:35:02 +0100

    add description to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,6 +36,10 @@ cities.each do |city|
     breed: Faker::Creature::Dog.breed,
     size: ['small', 'med-small', 'medium', 'med-large', 'large'].sample,
     category: ['guide dog', 'emotional support', 'kid-friendly', 'search and rescue'].sample,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.",
     city: city
   )
   dog.user = User.find(i)
